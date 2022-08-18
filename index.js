ebeed1e051c62116cc5bb8d2249e0b91c8b665f4
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-18 17:52:26 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("Walk")
+
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(Very='*'){
+    return function lab(tellMe="special"){
+        return `You are ${Very}${tellMe}${Very}!`
+    }
+}
